62fd444fe14ba26827032e34549cda94d7ea0c10
ahairshi <daisyfuentesahamed@gmail.com>
2016-09-29 15:11:54 +0530

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate');
     del = require('del');
 
 gulp.task('jshint', function() {
@@ -32,7 +33,9 @@ gulp.task('default', ['clean'], function() {
 
 gulp.task('usemin',['jshint'], function () {
     return gulp.src('./app/menu.html')
-        .pipe(usemin({css:[minifycss(),rev()],js: [uglify(),rev()]}))
+        .pipe(usemin({
+        css:[minifycss(),rev()],
+        js: [ngannotate(), uglify(),rev()]}))
         .pipe(gulp.dest('dist/'));
 });
 
